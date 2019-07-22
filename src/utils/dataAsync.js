1811384061e773cd05fae08902abb02160fdff87
benyasin <rgwapkk520>
2019-07-22 09:23:19 +0800

    fix bug

--- src/utils/dataAsync.js
+++ src/utils/dataAsync.js
@@ -19,7 +19,7 @@ async function getUserBasicInfo(phoneNum) {
       if (userInfo && userInfo.data && userInfo.data.data && userInfo.data.data.logo) {
         let logo = userInfo.data.data.logo;
         if (!logo.startsWith('http') && !logo.startsWith('data:image')) {
-          userInfo.data.data.logo = settings.imageUrl + logo + '&r=' + randomstring.generate(6);
+          userInfo.data.data.logo = settings.imageUrl + logo + '?t=' + randomstring.generate(6);
         }
       }
       if (userInfo && userInfo.data && userInfo.data.data) {
